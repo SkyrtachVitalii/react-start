009e0396a67820c4ec3bb76f11627fbb6e6c1984
Skyrtach Vitalii <vitaliiskirtac@gmail.com>
2023-09-25 13:05:18 +0300

    add new files to project

--- src/components/AsideMenu/AsideMenu.js
+++ src/components/AsideMenu/AsideMenu.js
@@ -6,10 +6,10 @@ function AsideMenu() {
     <div className="AsideMenu">
       <nav>
         <ul className='AsideMenu__List'>
-          <li className='AsideMenu__Item'><NavLink to="/" activeclassname="active">Home</NavLink></li>
-          <li className='AsideMenu__Item'><NavLink to="/products" activeclassname="active">Products</NavLink></li>
-          <li className='AsideMenu__Item'><NavLink to="/contacts" activeclassname="active">Contacts</NavLink></li>
-          <li className='AsideMenu__Item'><NavLink to="/cabinet" activeclassname="active">Cabinet</NavLink></li>
+          <li className='AsideMenu__Item'><NavLink to="/" className="AsideMenu__Link" activeclassname="active">Home</NavLink></li>
+          <li className='AsideMenu__Item'><NavLink to="/products" className="AsideMenu__Link" activeclassname="active">Products</NavLink></li>
+          <li className='AsideMenu__Item'><NavLink to="/contacts" className="AsideMenu__Link" activeclassname="active">Contacts</NavLink></li>
+          <li className='AsideMenu__Item'><NavLink to="/cabinet" className="AsideMenu__Link" activeclassname="active">Cabinet</NavLink></li>
         </ul>
       </nav>
     </div>
